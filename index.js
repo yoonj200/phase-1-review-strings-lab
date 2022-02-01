e153310e9346d55dc80035ba6c39cd931805a43f
Joshua <yoonj200@gmail.com>
2022-02-01 12:35:47 -0600

    commit tree 90ec058864bde015dea1034643c2f2cc2f57f0a7

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+// currentUser
+let currentUser = `Bob`;
+
+// welcomeMessage
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// excitedWelcomeMessage
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// shortGreeting
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
